e172483f55c2f68448be318bb2336a5c5250df03
Daniel Brice <danielbrice@gmail.com>
2018-05-11 02:59:22 -0700

    refactor

--- Shuffled.hs
+++ Shuffled.hs
@@ -2,6 +2,7 @@ module Shuffled (isShuffledNaive, isShuffledDynamic, isShuffledConcurrent) where
 
 import Control.Concurrent.Async (race)
 import Control.Monad (guard)
+import Data.Function (fix)
 import Data.List (tails, find)
 
 isShuffledConcurrent :: Eq a => ([a],[a]) -> [a] -> IO Bool
@@ -10,28 +11,31 @@ isShuffledConcurrent (left,right) combined =
     naive = return (isShuffledNaive (left,right) combined)
     dynamic = return (isShuffledDynamic (left,right) combined)
 
-go :: (([a],[a],[a]) -> [Bool]) -> ([a],[a],[a]) -> [Bool]
-go f (l:ls,r:rs,c:cs) | l == c && r == c = f (ls,r:rs,cs) ++ f (l:ls,rs,cs)
-go f (l:ls,  rs,c:cs) | l == c           = f (ls,rs,cs)
-go f (  ls,r:rs,c:cs) |           r == c = f (ls,rs,cs)
-go _ (  [],  [],  [])                    = [True]
-go _                _                    = [False]
+step :: Eq a => (([a],[a],[a]) -> [Bool]) -> ([a],[a],[a]) -> [Bool]
+step f (l:ls,r:rs,c:cs) | l == c && r == c = f (ls,r:rs,cs) ++ f (l:ls,rs,cs)
+step f (l:ls,  rs,c:cs) | l == c           = f (ls,rs,cs)
+step f (  ls,r:rs,c:cs) |           r == c = f (ls,rs,cs)
+step _ (  [],  [],  [])                    = [True]
+step _                _                    = [False]
+
+run :: (([a],[a],[a]) -> [Bool]) -> ([a],[a],[a]) -> Bool
+run f (ls,rs,cs) =
+  if length ls + length rs /= length cs then False
+  else or (f (ls,rs,cs))
 
 isShuffledNaive :: Eq a => ([a],[a]) -> [a] -> Bool
-isShuffledNaive (left,right) combined = or (go (fix go) (left,right,combined))
+isShuffledNaive (ls,rs) cs = run (fix step) (ls,rs,cs)
 
 isShuffledDynamic :: Eq a => ([a],[a]) -> [a] -> Bool
-isShuffledDynamic (left,right) combined =
-  if length left + length right /= length combined then False else
-  or (go (go' !) (left,right,combined)) where
-    go' :: [(a,[Bool])]
-    go' = [(x,go x) | x <- subproblems]
-    subproblems :: [([a],[a],[a])]
-    subproblems = do
-      ls <- tails left
-      rs <- tails right
-      cs <- tails combined
-      guard (length ls + length rs == length cs)
-      return (ls,rs,cs)
-    (!) :: [(a,[Bool])] -> a -> [Bool]
-    xys ! x = case find (\(x',_) -> x' == x) xys of Just (_,y) -> y
+isShuffledDynamic (ls,rs) cs = run step' (ls, rs, cs) where
+  step' = step (get store)
+  store = [ (x,step' x) | x <- subproblems (ls,rs,cs) ]
+  get store x0 = head [ y | (x,y) <- store, x == x0 ]
+
+subproblems :: ([a],[a],[a]) -> [([a],[a],[a])]
+subproblems (ls,rs,cs) = do
+  ls' <- tails ls
+  rs' <- tails rs
+  cs' <- tails cs
+  guard (length ls' + length rs' == length cs')
+  return (ls',rs',cs')
